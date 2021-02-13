40eecbfe68c59f00e16bab51eba2f5911b78f8db
Beyzv <77547830+Beyzv@users.noreply.github.com>
2021-02-14 00:01:11 +0300

    14,02,2021

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -20,6 +20,7 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+
         public IResult Add(Product product)
         {
             if (product.ProductName.Length<2)
@@ -32,9 +33,9 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour == 22)
-            {
-                return new ErrorDataResult<List<Product>>(Messages.MainenanceTime);
+          if (DateTime.Now.Hour == 22)
+           {
+             return new ErrorDataResult<List<Product>>(Messages.MainenanceTime);
             }
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductsListed);
         }
